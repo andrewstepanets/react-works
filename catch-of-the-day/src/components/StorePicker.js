879d825f73ecfdbc97bb1147c4bc70b76f910b3e
Andrii Stepanets <ovrass@gmail.com>
2020-03-31 23:40:27 +0300

    working with HTML in JSX

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>I am a Store Picker</p>
+        return (
+            // return back just one element
+            // if you want to return two adjacent elements
+            // you can use React.Fragment
+            // <Fragment>
+            // <p>Fish!</p>
+            //     <form className="store-selector">
+            //         <h2>Please Enter A Store</h2>
+            //     </form>
+            // </Fragment>
+            
+            <form className="store-selector">
+                <h2>Please Enter A Store</h2>
+                <input type="text" required placeholder="store Name"/>
+                <button type="submit">Visit store</button>
+            </form>
+        )
     }
 }
 
