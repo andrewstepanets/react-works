04a5cd81ea7c7e1efc04d07abb6b0c136ac72aa4
Andrii Stepanets <ovrass@gmail.com>
2020-06-01 20:33:03 +0300

    refactoring base js

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,9 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBiEc76qUfScCIJjdG-dfaXKDtdaiaPYXs",
-    authDomain: "catch-of-the-day-andrew-3c744.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-andrew-3c744.firebaseio.com"
+    
 })
 
 const base = Rebase.createClass(firebaseApp.database());
