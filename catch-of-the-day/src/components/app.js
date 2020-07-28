2c2e6e79708b31a36f5a8c2678bcffc6f58168ec
Andrii Stepanets <ovrass@gmail.com>
2020-07-28 15:38:57 +0300

    created header, order and inventory components

---
+++ catch-of-the-day/src/components/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './header';
+import Inventory from './inventory';
+import Order from './order';
+
+class App extends React.Component {
+    render(){
+        return(
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header />
+                </div>
+                <Order />
+                <Inventory />
+            </div>
+        )
+    }
+}
+
+export default App;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/store-picker';
+import App from './components/app';
 import './css/style.css';
 
 
 
-render(<StorePicker />, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
