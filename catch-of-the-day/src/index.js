d640bed0ed7c5cf64dcc400bfe200998cf36c7db
Andrii Stepanets <ovrass@gmail.com>
2020-07-28 15:02:44 +0300

    Created first form and add css file

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/store-picker'
+import StorePicker from './components/store-picker';
+import './css/style.css';
 
 
 
